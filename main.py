816f86b67a574b9fec6f6c88b2a34a8997337d3e
rkibistu <ogreanrazvan@gmail.com>
2024-04-29 14:03:32 +0300

    [STEGANIGRAPHY]-[New method: hide data in metadata]
    stored using exif format

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import steganography as steg
 
 cover_img_path = "images/cover.jpg"
 
+
 def main():
-    steg.append_after(cover_img_path,"My homeowrk here!")
-    test = steg.extract_append_after(cover_img_path)
+    # steg.append_after(cover_img_path,"My homeowrk here!")
+    # test = steg.extract_append_after(cover_img_path)
+    
+    steg.hide_inside_metadata(cover_img_path,"images/metadata_secret.jpg","wuhuu, it is working!")
+    test = steg.extract_from_metada("images/metadata_secret.jpg")
     print(test)
 
 if __name__ == "__main__":
--- steganography.py
+++ steganography.py
@@ -1,9 +1,13 @@
+import piexif
+from PIL import Image
+import numpy as np
 
-# encod message in binary and append it to the end of image file
+# VERY SIMPLE METHOD, EASY TO DETECT! -> should encrypt the message
+# Encode message in binary and append it to the end of image file
 def append_after(filename, message):
     with open(filename,"ab") as f:
         f.write(message.encode('utf8'))
-# extract message appended at the end of a JPG file        
+# Extract message appended at the end of a JPG file        
 def extract_append_after(filename):
     trailer = 'ffd9' # trailer for JPEG
 
@@ -17,5 +21,32 @@ def extract_append_after(filename):
         cover_secret.seek(offset + len(trailer)//2)
         return cover_secret.read()
     
+# Hides the secret in the image metadata (stored in the exif format): description
+# (exif metadata is restricted in size and most paltforms will remove it on upload)
+# More exif flags: https://github.com/hMatoba/Piexif/blob/master/piexif/_exif.py
+#   piexif.ImageIFD.ImageDescription - change ImageDescription with anotehr tag 
+#   to hide message on another place
+def hide_inside_metadata(cover_filename, result_filename, message):
+    im = Image.open(cover_filename)
+    #check if exif dictionary is avaialble
+    if "exif" in im.info:
+        #load exif dictionary
+        exif_dict = piexif.load(im.info["exif"])
+        #modify the value of description from the dictionary
+        exif_dict["0th"][piexif.ImageIFD.ImageDescription] = message
+        #convert back to specific format
+        exif_bytes = piexif.dump(exif_dict)
+    else:
+        #create a exif dictionary
+        exif_bytes = piexif.dump({"0th":{piexif.ImageIFD.ImageDescription:message}})
 
-          
+    #write modified image
+    im.save(result_filename, exif=exif_bytes)
+
+def extract_from_metada(filename):
+    im = Image.open(filename)
+    result = piexif.load(im.info["exif"])["0th"][piexif.ImageIFD.ImageDescription].decode("utf-8")
+    return result
+
+
+
