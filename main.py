c19571fa86518be7c2eabc1758216f01c3d883ce
rkibistu <ogreanrazvan@gmail.com>
2024-04-29 14:16:57 +0300

    [STEGANOGRAPHY]-[New method: hide message isnide lsb]

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import steganography as steg
 
-cover_img_path = "images/cover.jpg"
+cover_img_path1 = "images/cover.jpg"
+cover_img_path2 = "images/dog.png"
 
 
 def main():
-    # steg.append_after(cover_img_path,"My homeowrk here!")
+    # steg.append_after(cover_img_path1,"My homeowrk here!")
     # test = steg.extract_append_after(cover_img_path)
     
-    steg.hide_inside_metadata(cover_img_path,"images/metadata_secret.jpg","wuhuu, it is working!")
-    test = steg.extract_from_metada("images/metadata_secret.jpg")
+    # steg.hide_inside_metadata(cover_img_path1,"images/metadata_secret.jpg","wuhuu, it is working!")
+    # test = steg.extract_from_metada("images/metadata_secret.jpg")
+    
+    steg.hide_message_inside_lsb(cover_img_path2,"images/lsb_msg_image.png","hello from lsb!")
+    test = steg.exctract_messge_from_lsb("images/lsb_msg_image.png",'!')
     print(test)
 
 if __name__ == "__main__":
--- steganography.py
+++ steganography.py
@@ -50,3 +50,62 @@ def extract_from_metada(filename):
 
 
 
+# Hide message inside LSB of the image
+# JPG is problematic with this because LSB may be lost during compression.
+#   use: png, gif, bitmap
+def hide_message_inside_lsb(filename, result_filename, message, end_of_message = None):
+    
+    # add a character to amrk end of message
+    if(end_of_message != None):
+        message += end_of_message
+    
+    # Encode the message in a serie of 8-bit values (covnert to binary)
+    b_message = ''.join(["{:08b}".format(ord(x)) for x in message ])
+    b_message = [int(x) for x in b_message]
+
+    b_message_lenght = len(b_message)
+
+    # Get the image pixel arrays 
+    with Image.open(filename) as img:
+        width, height = img.size
+        data = np.array(img)
+        
+    # Flatten the pixel arrays (only one array)
+    data = np.reshape(data, width*height*4)
+
+    # Overwrite pixel LSB
+    data[:b_message_lenght] = data[:b_message_lenght] & ~1 | b_message
+
+    # Reshape back to an image pixel array
+    data = np.reshape(data, (height, width, 4))
+
+    new_img = Image.fromarray(data)
+    new_img.save(result_filename)
+
+# end_of_message is the character that marsk the end of the message
+#   if not present, read until see an unprintable value
+def exctract_messge_from_lsb(filename, end_of_message = None):
+    with Image.open(filename) as img:
+        width, height = img.size
+         # Get the image pixel arrays 
+        data = np.array(img)
+    
+    # Flatten the pixel arrays (only one array)
+    data = np.reshape(data, width*height*4)
+    
+    # extract lsb
+    data = data & 1 
+    
+    # Packs binary-valued array into 8-bits array.
+    data = np.packbits(data)
+    
+    # Read and convert integer to Unicode characters until hitting a non-printable character
+    result = ''
+    for x in data:
+        l = chr(x)
+        if(end_of_message != None and l == end_of_message):
+            break
+        elif not l.isprintable():
+            break
+        result += l
+    return result
